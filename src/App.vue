a18e190d67088d4c290be4462d559a28d4470c3e
mwritter <mritter2283@gmail.com>
2019-10-05 21:58:07 -0400

    Added Profile component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
   <div id="app">
     <div id="profile_picture"></div>
     <h3>Hi my name's Matthew Ritter</h3>
+    <Profile />
   </div>
 </template>
 
 <script>
+import Profile from "./components/Profile";
 export default {
   name: "app",
-  components: {}
+  components: {
+    Profile
+  }
 };
 </script>
 
@@ -20,6 +24,8 @@ export default {
 }
 #app {
   display: grid;
+  justify-items: center;
+  padding: 3rem 2rem;
   height: 100vh;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -34,8 +40,17 @@ export default {
   background: url("./assets/profile_picture.jpg") no-repeat;
   background-size: cover;
   background-position: center;
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
+}
+.info-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  background-color: rgba(232, 232, 232, 0.3);
+  backdrop-filter: blur(10px);
+  border-radius: 20px;
 }
 </style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="info-section">
+    <p>
+      I'm a creator. I like to build things and I use software development to do just that. These are the technologies I'm currently working with and have used in my own projects. I'm always
+      finding new tools to solve problems, which means I'm always learning new things and I love that about software development.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
